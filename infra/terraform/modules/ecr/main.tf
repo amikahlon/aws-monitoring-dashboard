c994fe70fe046625a8507eec32cb1e4b560e30b3
Ami Kahlon <amikahlon2@gmail.com>
2026-03-10 15:48:36 +0200

    try

--- infra/terraform/modules/ecr/main.tf
+++ infra/terraform/modules/ecr/main.tf
@@ -2,6 +2,9 @@ resource "aws_ecr_repository" "this" {
   name                 = var.repository_name
   image_tag_mutability = var.image_tag_mutability
 
+  # מאפשר למחוק את ה-repository גם אם יש בו images
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
